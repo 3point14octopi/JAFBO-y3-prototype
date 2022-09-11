60f75a590af14ae3f4dda0bfe8408c194114b1c9
Isaac <quatchimanisaac@gmail.com>
2022-09-11 16:34:51 -0400

    Prefab representation of arena

--- Assets/Scripts/MakeArenaArray.cs
+++ Assets/Scripts/MakeArenaArray.cs
@@ -9,6 +9,8 @@ public class MakeArenaArray : MonoBehaviour
 {
     public Space2D arena;
     public TextMeshProUGUI iAmLessSad;
+    public List<GameObject> prefabs = new List<GameObject> { };
+    public Transform boardOrigin;
     public bool OhNo = true;
     // Start is called before the first frame update
     void Start()
@@ -19,19 +21,22 @@ public class MakeArenaArray : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.anyKeyDown)
+        if(Input.GetKeyDown(KeyCode.Space))
         {
             if (OhNo)
             {
                 ArenaGenFunctions_Prototype.ArenaPrototype(arena);
-                PrintToTextMesh();
-            }else
+               // PrintToTextMesh();
+                DisplaySpace2D();
+            }
+            else
             {
                 Space2D myEntireAss = new Space2D(28, 28);
                 BasicBuilderFunctions.Flood(myEntireAss, new Cell(0), new Cell(1));
                 myEntireAss.worldOrigin = new Coord(1, 1);
                 BasicBuilderFunctions.CopySpaceAToB(myEntireAss, arena, new List<Cell> { });
-                PrintToTextMesh();
+             //   PrintToTextMesh();
+                DisplaySpace2D();
             }
             OhNo = !OhNo;
         }
@@ -47,6 +52,33 @@ public class MakeArenaArray : MonoBehaviour
                 iAmLessSad.text =  iAmLessSad.text + (arena.GetCell(new Coord(j, i))) + " ";
             }
             iAmLessSad.text = iAmLessSad.text + "\n";
+        }
+    }
+
+    protected void DisplaySpace2D()
+    {
+
+        Infanticide(boardOrigin.transform);
+        
+        for (int i = 0; i < arena.height; i++)
+        {
+            for (int j = 0; j < arena.width; j++)
+            {
+
+                GameObject currentTile = Instantiate(prefabs[arena.GetCell(new Coord(j,i))], new Vector3(boardOrigin.position.x + j, 0, boardOrigin.position.z - i), Quaternion.identity);
+                currentTile.transform.SetParent(boardOrigin);
+                //iAmLessSad.text = iAmLessSad.text + (arena.GetCell(new Coord(j, i))) + " ";
+
+            }
+            iAmLessSad.text = iAmLessSad.text + "\n";
+        }
+    }
+
+    protected void Infanticide(Transform parent)
+    {
+        foreach (Transform child in parent)
+        {
+            GameObject.Destroy(child.gameObject);
         }
     }
 }
